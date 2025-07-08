587a199382c4439b0fffff2ced792a5a94bdd744
Eli Levi <el1l3vi@gmail.com>
2025-07-08 23:55:35 +0300

    server side can run bash command & prep json from files

--- client_side.py
+++ client_side.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+from SshToServer import SshToServer
+
 
 def append_to_csv(file_path, data):
 	df_new = pd.DataFrame([data])
@@ -9,3 +11,15 @@ def append_to_csv(file_path, data):
 	else:
 		df_combined = df_new
 	df_combined.to_csv(file_path, index=False)
+
+
+def main():
+	user_pem = input("Write the absolute path to your pem file: ")
+	my_ssh = SshToServer(user_pem, "13.51.195.198", "ubuntu")
+	my_ssh.connect()
+	response = my_ssh.runRemoteCommand("python3 server_side.py")
+
+
+
+if __name__ == "__main__":
+	main()
--- server_side.py
+++ server_side.py
@@ -1,2 +1,46 @@
 import json
+import os
+import pandas as pd
+import subprocess
+
+
 log_path = "/var/log/syslog"
+severities_json = {"WARN": 0, "INFO": 0, "ERROR": 0}
+files_list =["warn.txt", "info.txt", "error.txt"]
+
+def run_local_command(command):
+    try:
+        # Run the command
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout, result.stderr
+
+    except subprocess.CalledProcessError as e:
+        print(f"Command '{command}' returned non-zero exit status {e.returncode}")
+        print(f"Error output: {e.stderr}")
+
+def prepJsonFiles():
+    for file in files_list:
+        severity_type = file.strip(".txt").upper()
+        absulute_file_path = os.getcwd() +"\\"+ file
+        severity_counter = 0
+        with open(absulute_file_path, 'r') as severity_file: 
+            content = severity_file.readlines()
+            for line in content:  
+                severity_counter += 1
+        severities_json[severity_type] = severity_counter
+    return severities_json
+
+def createSeverityLogs():
+    for severity in severities_json.keys():
+        file_name = severity.lower() + ".txt"
+        files_list.append(file_name)
+        run_local_command(f'grep {severity} {log_path} > {file_name}')
+
+
+def main():
+    res = prepJsonFiles()
+
+
+
+if __name__ == "__main__":
+    main()
